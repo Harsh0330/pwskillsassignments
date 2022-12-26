70e77f90926135eeb6a4c0ed5d939b2e44393c35
Harsh <cheenuharsh@gmail.com>
2022-12-26 20:07:46 +0530

    .

--- C++ dsa/Leacture and notes/Leacture 8 - Problems on Loops - Part 2 -  Urvi Goel/Testing/assignment.cpp
+++ C++ dsa/Leacture and notes/Leacture 8 - Problems on Loops - Part 2 -  Urvi Goel/Testing/assignment.cpp
@@ -261,15 +261,14 @@ int main()
     // }
 
     // 10)
-    // test
-    int i, j, a, b;
-    cin >> a >> b;
-    for (i = 1; i <= a; i++)
-    {
-        for (j = 1; j <= b; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    // int i, j, a, b;
+    // cin >> a >> b;
+    // for (i = 1; i <= a; i++)
+    // {
+    //     for (j = 1; j <= b; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
 }
