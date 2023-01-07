bfa2f91cfbb7c0aa50e770ce25ec6d961e41d2a4
Harsh <cheenuharsh@gmail.com>
2023-01-07 11:40:50 +0530

    .

---
+++ C++ dsa/Leacture and notes/Leacture 12 - Functions in C++ 2 -  Urvi Goel/Testing/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int p = 10;
+    int i=5;
+    while (i!=0)
+    {
+        float p = 10.5;
+        cout << p << endl;
+        i--;
+    }
+    cout << p;
+}
---
+++ C++ dsa/Leacture and notes/Leacture 13 - Array Creation, Types, and Operations -  Urvi Goel/Testing/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int array[5] = {1, 2, 3, 4, 5};
+    cout << sizeof(array) << endl;
+}
